51aef9859132b02a9c844b0a7c34c18bc787d289
Wanderlust <74563094+lmnl01@users.noreply.github.com>
2025-02-13 22:34:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -56,14 +56,14 @@
 const messages = [
     "Are you sure?",
     "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "SURE KA NAAAAAA?",
+    "Uyyyy please...",
+    "Sige na kaseeee huhuhu",
+    "Iiyak ako dito...",
+    "LABSSSSSSSS...",
+    "Eto na, paiyak na akooo...",
+    "Ok fine, wag nalang siguro...",
+    "Jokeeeee, say yes naaaa please! mwaaaah"
 ];
 
 let messageIndex = 0;
